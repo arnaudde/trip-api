8dfc147239b74dbc9498d01df2e04704830bbf7c
Arnaud <arnaud.d'esquerre@lifeworks.com>
2018-03-24 11:13:00 +0000

    Add json error for getting the city

--- app/controllers/CityController.java
+++ app/controllers/CityController.java
@@ -8,6 +8,8 @@ import play.data.validation.Constraints;
 import play.mvc.*;
 import static play.libs.F.Tuple;
 import javax.inject.Inject;
+import java.util.Map;
+import java.util.HashMap;
 
 import static play.libs.Json.toJson;
 
@@ -19,8 +21,32 @@ public class CityController extends Controller {
         return ok(toJson(City.findAll()));
     }
 
-    public Result getCity(Long id) { return ok(toJson(City.find(id))); }
-    public Result getCityByName(String name) { return ok(toJson(City.findByName(name))); }
+    public Result getCity(Long id) {
+        City city = City.find(id);
+        if (city != null) {
+            return ok(toJson(city));
+        } else {
+            Map<String, Map<String, String>> error = new HashMap();
+            Map<String, String> fields = new HashMap();
+            fields.put("message", "No city was found for this id");
+            fields.put("id", String.valueOf(id));
+            error.put("error", fields);
+            return badRequest(toJson(error));
+        }
+    }
+    public Result getCityByName(String name) {
+        City city = City.findByName(name);
+        if (city != null) {
+            return ok(toJson(city));
+        } else {
+            Map<String, Map<String, String>> error = new HashMap();
+            Map<String, String> fields = new HashMap();
+            fields.put("message", "No city was found for this name");
+            fields.put("name", String.valueOf(name));
+            error.put("error", fields);
+            return badRequest(toJson(error));
+        }
+    }
 
     public Result createCity() {
         Form<City> form = formFactory.form(City.class).bindFromRequest();
