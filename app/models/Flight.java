4438901e28d219f190ed52bd1610c96d1d467573
Arnaud <arnaud.d'esquerre@lifeworks.com>
2018-03-24 12:53:09 +0000

    Add getters and setters to Flight

--- app/models/Flight.java
+++ app/models/Flight.java
@@ -23,19 +23,15 @@ public class Flight extends Model {
     @Constraints.Required
     public String departure;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    @Constraints.Required
-    public Date date;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    public Date created_on;
-
     @Column(length = 3, nullable = false)
     @Constraints.MaxLength(3)
     @Constraints.Required
     public String arrival;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    @Constraints.Required
+    public Date date;
 
     @Column(length = 4, nullable = false)
     @Constraints.MaxLength(4)
@@ -56,6 +52,81 @@ public class Flight extends Model {
     @Constraints.Required
     public Integer price;
 
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    public Date created_on;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMin_departure_time() {
+        return min_departure_time;
+    }
+
+    public void setMin_departure_time(String min_departure_time) {
+        this.min_departure_time = min_departure_time;
+    }
+
+    public String getMax_departure_time() {
+        return max_departure_time;
+    }
+
+    public void setMax_departure_time(String max_departure_time) {
+        this.max_departure_time = max_departure_time;
+    }
+
+    public String getMax_duration() {
+        return max_duration;
+    }
+
+    public void setMax_duration(String max_duration) {
+        this.max_duration = max_duration;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Date getCreated_on() {
+        return created_on;
+    }
+
+    public void setCreated_on(Date created_on) {
+        this.created_on = created_on;
+    }
 
     public static List<Flight> findAll() {
         Finder<Long, Flight> finder = new Finder<>(Flight.class);
