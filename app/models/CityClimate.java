2c9ef29730fdfae7a6233c92dc9b33e52e770faa
Arnaud <arnaud.d'esquerre@lifeworks.com>
2018-03-24 12:17:47 +0000

    Add getters and setters to cityClimate to get rid of Bean property city_id is not readable or has an invalid getter method

--- app/models/CityClimate.java
+++ app/models/CityClimate.java
@@ -7,7 +7,6 @@ import io.ebean.*;
 import play.data.format.*;
 import play.data.validation.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -27,7 +26,6 @@ public class CityClimate extends Model {
     @Column(nullable =  false)
     @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
     @JsonIdentityReference(alwaysAsId=true)
-    @JsonProperty("city_id")
     public City city;
 
     @Constraints.Required
@@ -74,9 +72,88 @@ public class CityClimate extends Model {
     @Constraints.Required
     public Integer score;
 
+    public City getCity() {
+        return city;
+    }
 
     public static List<CityClimate> findAll() {
         Finder<Long, CityClimate> finder = new Finder<>(CityClimate.class);
         return finder.all();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Long getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(Long city_id) {
+        this.city_id = city_id;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getMean_temperature() {
+        return mean_temperature;
+    }
+
+    public void setMean_temperature(Integer mean_temperature) {
+        this.mean_temperature = mean_temperature;
+    }
+
+    public Integer getMin_mean_temperature() {
+        return min_mean_temperature;
+    }
+
+    public void setMin_mean_temperature(Integer min_mean_temperature) {
+        this.min_mean_temperature = min_mean_temperature;
+    }
+
+    public Integer getMax_mean_temperature() {
+        return max_mean_temperature;
+    }
+
+    public void setMax_mean_temperature(Integer max_mean_temperature) {
+        this.max_mean_temperature = max_mean_temperature;
+    }
+
+    public Integer getPrecipitations() {
+        return precipitations;
+    }
+
+    public void setPrecipitations(Integer precipitations) {
+        this.precipitations = precipitations;
+    }
+
+    public Integer getNumber_days_of_rain() {
+        return number_days_of_rain;
+    }
+
+    public void setNumber_days_of_rain(Integer number_days_of_rain) {
+        this.number_days_of_rain = number_days_of_rain;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
     }
 }
